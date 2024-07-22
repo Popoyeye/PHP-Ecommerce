cb0e2a1e7d9393d29630ff427906cf7591d42139
Izham <168050081+Popoyeye@users.noreply.github.com>
2024-07-22 21:43:30 +0800

    download.php

--- download.php
+++ download.php
@@ -39,7 +39,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'izhamzam27@gmail.com';                     //SMTP username
-    $mail->Password   = 'epmfdacnouzmhanc';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
